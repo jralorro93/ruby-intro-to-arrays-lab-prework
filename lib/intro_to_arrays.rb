9000699bc17c67c9c825a5a5d260d9bca7c896dd
Juan Alorro <jralorro93@gmail.com>
2019-01-24 19:09:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,3 +18,11 @@ end
 def last_element(array)
   array.last
 end 
+
+def first_element_with_array_methods(array)
+  array.shift
+end 
+
+def last_element_with_array_methods(array)
+  array.pop
+end 
